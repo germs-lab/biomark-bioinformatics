5d5ba86c7461ebc73bf54dc0e1ff4d2c48e337c7
Adina Howe <adinahowe@Adinas-Air.san.rr.com>
2020-05-29 08:57:34 -0500

    changes

--- combine-biomark-csv.py
+++ combine-biomark-csv.py
@@ -3,11 +3,9 @@ import sys
 #  python combine-biomark-csv.py *csv  > combined.csv
 
 for n, f in enumerate(sys.argv[1:]):
-    if n == 1:
-        for m, line in enumerate(open(f)):
-            if m > 10:
-                print(line.rstrip())
-    else:
-        for m, line in enumerate(open(f)):
-            if m > 11:
-                print(line.rstrip())
+    for m, line in enumerate(open(f)):
+        dat = line.rstrip().split(',')
+        dat.append(f.split()[0])
+        print(','.join(dat))
+
+    
