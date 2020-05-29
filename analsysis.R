ed81d8506d24f9594baf5a4ed570798bd8032ea1
Adina Howe <adina@users.noreply.github.com>
2020-05-29 10:09:59 -0500

    Add files via upload

---
+++ analsysis.R
@@ -0,0 +1,132 @@
+#devtools::install_github("sjackman/uniqtag")
+library(plyr)
+library(uniqtag)
+library(ggplot2)
+
+setwd("~/Downloads/biomark-private/")
+raw_data_file = "combined.cleaned.csv"
+raw_data <- read.csv(raw_data_file, sep=",", header = FALSE)
+header = c('ID','Name','Type','rConc','Name.1','Type','Value','Calibrated rConc','Quality','Call','Threshold','In Range','Out Range','Peak Ratio', 'Plate')
+colnames(raw_data) <- header
+dim(raw_data)
+head(raw_data)
+raw_data$full_assay <- paste(raw_data$Name,"-", raw_data$Name.1, sep="")
+raw_data$unique_id <- make_unique(raw_data$full_assay)
+
+
+# Metadata fie has to have a header names "sample_type" where you specify standards or env sample
+# You should have metadata for both samples and standards
+# It would be better if the "standard sample" had the appropriate dilution
+meta_data_file = "meta/Worle_biomark_meta.cleaned.csv"
+meta <- read.csv(meta_data_file, header=TRUE)
+
+
+# Check the NTCs
+ct_threshold_ntc = 20
+ntc <- subset(raw_data, Name == "NTC")
+summary(ntc$Value)
+check_ntc <- subset(ntc, Value > 20 & Value < 999)
+check_ntc$Value
+data_nontc <- subset(raw_data, Name != "NTC")
+
+merged <- merge(data_nontc, meta, by = "Name")
+
+
+#Standards Workflow
+
+standards <- subset(merged, sample_type == "standard")
+gene_list <- unique(standards$Name)
+
+for (x in 1:length(gene_list)) {
+   each_gene <- subset(standards, Name == gene_list[x])
+   standard_only = paste(gene_list[x], '-', gene_list[x], sep='')
+   each_gene_filtered = subset(each_gene, full_assay == standard_only)
+   each_gene_filtered = subset(each_gene_filtered, Value < 500)
+   print(each_gene_filtered)
+   each_gene_badfiltered = subset(each_gene, full_assay != standard_only)
+   each_gene_badfiltered = subset(each_gene_badfiltered, Value < 999)
+   print(each_gene_badfiltered)
+   f <- ddply(each_gene_filtered, .(Plate, rConc, full_assay), summarise, MEAN = mean(Value), SE=sd(Value)/sqrt(length(Value)))
+   limits<-aes(ymin=MEAN-SE, ymax=MEAN+SE)
+   f$log <- log10(f$rConc)
+   p = ggplot(f, aes_string(x="log", y="MEAN", color="Plate"))
+   p = p + geom_point() + geom_errorbar(position=position_dodge(), limits)+theme(axis.text.x = element_text(angle = 90))
+   p
+   ggsave(paste0(x,'standard.pdf'), device="pdf")
+  #f <- ddply(each_gene_badfiltered, .(rConc, full_assay), summarise, MEAN = mean(Value), SE=sd(Value)/sqrt(length(Value)))
+  #limits<-aes(ymin=MEAN-SE, ymax=MEAN+SE)
+  #f$log <- log10(f$rConc)
+  #p = ggplot(f, aes_string(x="log", y="MEAN", color="full_assay"))
+  #p = p + geom_point() + geom_errorbar(position=position_dodge(), limits)+theme(axis.text.x = element_text(angle = 90))
+  #p + facet_grid(~Plate)
+  #ggsave(paste0(x,'nonstandard.pdf'), device="pdf")
+         
+}
+
+# Here is where you define the index of the gene you are looking for the standards for x = index in gene_list
+x = 7
+ {
+  each_gene <- subset(standards, Name == gene_list[x])
+  standard_only = paste(gene_list[x], '-', gene_list[x], sep='')
+  each_gene_filtered = subset(each_gene, full_assay == standard_only)
+  each_gene_filtered = subset(each_gene_filtered, Value < 500)
+  #print(each_gene_filtered)
+  each_gene_badfiltered = subset(each_gene, full_assay != standard_only)
+  each_gene_badfiltered = subset(each_gene_badfiltered, Value < 999)
+  #print(each_gene_badfiltered)
+  f <- ddply(each_gene_filtered, .(Plate, rConc, full_assay), summarise, MEAN = mean(Value), SE=sd(Value)/sqrt(length(Value)))
+  limits<-aes(ymin=MEAN-SE, ymax=MEAN+SE)
+  f$log <- log10(f$rConc)
+  p = ggplot(f, aes_string(x="log", y="MEAN", color="Plate"))
+  p = p + geom_point() + geom_errorbar(position=position_dodge(), limits)+theme(axis.text.x = element_text(angle = 90)) 
+  p 
+ }
+
+#plots for our proposal
+samples <- subset(merged, sample_type != "standard") #all samples
+water <- subset(samples, sample_type == "water")
+water <- subset(water, Value < 100)
+f <- ddply(water, .(full_assay, plot, treatment, Name, Name.1), summarise, MEAN = mean(Value), SE=sd(Value)/sqrt(length(Value)))
+#genes that are detected #stats are done prior to removing bad measurements
+f_detects <- subset(f, MEAN < 100)
+print("Number of Genes with detects:  ")
+length(unique(f_detects$Name.1))
+unique(f_detects$Name.1)
+f_plot <-ddply(f, .(treatment, Name.1), summarise, MEAN2 = mean(MEAN), SE2=sd(MEAN)/sqrt(length(MEAN)))
+f2 <- subset(f_plot, MEAN2 < 30) #Can set as appropriate - filter by MEAN technical rep Ct
+temp = subset(f2, treatment == "WCM")
+f2$Name.1 <- factor(f2$Name.1, levels=temp$Name.1[order(-temp$MEAN)])
+limits2<-aes(ymin=MEAN2-SE2, ymax=MEAN2+SE2)
+p = ggplot(f2, aes_string(x="Name.1", y="MEAN2", color="Name.1"))
+p + geom_point() + geom_errorbar(width=0.25, limits2)+theme_classic()+ theme(text=element_text(size=15, family="Helvetica"),axis.text.x = element_text(angle = 90), legend.position="none") + facet_grid(~treatment)
+
+#Need to divide values by 16S
+#Some code we'd run on the whole plate data to get 16S per sample for division - Abundance here is estimated
+smple_v<- unique(f2$Plate)
+k<- 1
+f2$housekeeping_abund<- NA
+housekeeper = "16S_Eub_338F_515R"
+for(k in 1:length(smple_v)) {
+  smple_r<-which(f2$Plate==smple_v[k]) #find rows in data frame that belong to a particular sample
+  smple_abun<-mean(f2[smple_r[which(f2$Gene[smple_r]==housekeeper)],"Abundance"]) #average abundance of housekeeper gene for a given sample
+  #can update function here if a different means of normalizing is desired (e.g., max)
+  f2$housekeeping_abund[smple_r]<-smple_abun #assign housekeeper abundance for sample to all rows from sample
+}
+
+
+
+samples <- subset(merged, sample_type != "standard") #all samples
+manure <- subset(samples, sample_type == "manure")
+crop <- subset(samples, treatment = "WCS")
+crop <- subset(crop, sample_type == "soil")
+crop <- subset(crop, soil_type == "crop")
+total <- rbind(crop, manure)
+total <- subset(total, Value < 100)
+f <- ddply(total, .(sample_type, Name.1), summarise, MEAN = mean(Value), SE=sd(Value)/sqrt(length(Value)))
+f#genes that are detected #stats are done prior to removing bad measurements
+
+temp = subset(f2, treatment == "WCM")
+f2$Name.1 <- factor(f2$Name.1, levels=temp$Name.1[order(-temp$MEAN)])
+limits<-aes(ymin=MEAN-SE, ymax=MEAN+SE)
+p = ggplot(f, aes_string(x="Name.1", y="MEAN", color = "sample_type"))
+p + geom_point() + geom_errorbar(width=0.25, limits)+theme_classic()+ theme(text=element_text(size=15, family="Helvetica"),axis.text.x = element_text(angle = 90)) 
